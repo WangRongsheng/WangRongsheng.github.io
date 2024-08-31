36edee34dc20b6b264b7c5412d6ec9ff448cbe68
WangRongsheng <fujingshusheng@gmail.com>
2024-08-31 14:53:17 +0800

    Update main.py

--- google_scholar_crawler/main.py
+++ google_scholar_crawler/main.py
@@ -10,8 +10,8 @@ name = author['name']
 author['updated'] = str(datetime.now())
 author['publications'] = {v['author_pub_id']:v for v in author['publications']}
 print(json.dumps(author, indent=2))
-os.makedirs('results', exist_ok=True)
-with open(f'results/gs_data.json', 'w') as outfile:
+os.makedirs('../assets/results', exist_ok=True)
+with open(f'../assets/results/gs_data.json', 'w') as outfile:
     json.dump(author, outfile, ensure_ascii=False)
 
 shieldio_data = {
@@ -19,5 +19,5 @@ shieldio_data = {
   "label": "citations",
   "message": f"{author['citedby']}",
 }
-with open(f'results/gs_data_shieldsio.json', 'w') as outfile:
+with open(f'../assets/results/gs_data_shieldsio.json', 'w') as outfile:
     json.dump(shieldio_data, outfile, ensure_ascii=False)
